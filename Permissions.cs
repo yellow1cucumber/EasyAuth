2135286eabfa0a7e373eaaabaccab482b30f3972
yellow1cucumber <sadovskiy1n@gmail.com>
2025-04-28 21:25:15 +0500

    Добавьте файлы проекта.

---
+++ Permissions.cs
@@ -0,0 +1,10 @@
+﻿namespace EasyAuth
+{
+    public static class Permissions
+    {
+        public const string CreateRoles = "permissions.create_roles";
+        public const string EditRoles = "permissions.edit_roles";
+        public const string EditPermissions = "permissions.edit_permissions";
+        public const string EditUsers = "permissions.edit_users";
+    }
+}
