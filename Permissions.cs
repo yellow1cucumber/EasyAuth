9996ebbbf5a974dd7681f64d44b85ead2b248235
yellow1cucumber <sadovskiy1n@gmail.com>
2025-04-28 21:36:24 +0500

    chore: add XML comments for permissions constants
    - Added XML documentation comments in Permissions.cs.
    - Each permission constant now has a description.
    - Improved code documentation for better clarity.

--- Permissions.cs
+++ Permissions.cs
@@ -1,10 +1,28 @@
 ﻿namespace EasyAuth
 {
+    /// <summary>
+    /// Provides constants for defining permissions used in the application.
+    /// </summary>
     public static class Permissions
     {
+        /// <summary>
+        /// Permission to create roles.
+        /// </summary>
         public const string CreateRoles = "permissions.create_roles";
+
+        /// <summary>
+        /// Permission to edit roles.
+        /// </summary>
         public const string EditRoles = "permissions.edit_roles";
+
+        /// <summary>
+        /// Permission to edit permissions.
+        /// </summary>
         public const string EditPermissions = "permissions.edit_permissions";
+
+        /// <summary>
+        /// Permission to edit users.
+        /// </summary>
         public const string EditUsers = "permissions.edit_users";
     }
 }
