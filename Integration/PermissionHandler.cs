6b175eea34e001a760e003506b185c794fa4f493
yellow1cucumber <sadovskiy1n@gmail.com>
2025-04-28 22:00:29 +0500

    feat: add permission handler for user authorization
    - Introduced a new `PermissionHandler` class that extends `AuthorizationHandler<PermissionRequirement>`.
    - Implements authorization requirements based on user permissions.
    - Added `HandleRequirementAsync` method to check user permissions and succeed the authorization context if permissions are granted.
    - Included necessary namespaces for authorization and user permissions handling.

---
+++ Integration/PermissionHandler.cs
@@ -0,0 +1,30 @@
+﻿using EasyAuth.Services.UserPermissions;
+
+using Microsoft.AspNetCore.Authorization;
+
+namespace EasyAuth.Integration
+{
+    /// <summary>
+    /// Handles authorization requirements based on user permissions.
+    /// </summary>
+    public sealed class PermissionHandler : AuthorizationHandler<PermissionRequirement>
+    {
+        /// <summary>
+        /// Handles the authorization requirement by checking if the user has the required permission.
+        /// </summary>
+        /// <param name="context">The authorization context containing the user and resource information.</param>
+        /// <param name="requirement">The permission requirement to evaluate.</param>
+        /// <returns>A completed task representing the operation.</returns>
+        protected override Task HandleRequirementAsync(AuthorizationHandlerContext context, PermissionRequirement requirement)
+        {
+            var userPermissions = new UserPermissionService(context.User);
+
+            if (userPermissions.HasPermission(requirement.Permission))
+            {
+                context.Succeed(requirement);
+            }
+
+            return Task.CompletedTask;
+        }
+    }
+}
