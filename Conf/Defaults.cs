e6d335341c276557b91e81b98fed75855a9d0e20
yellow1cucumber <sadovskiy1n@gmail.com>
2025-04-28 21:36:39 +0500

    feat: add default configuration class for EasyAuth
    - Added a new static class `Defaults` in Defaults.cs.
    - The class provides default configuration values for the EasyAuth library.
    - Introduced a constant `PermissionClaimType` for storing permissions with the value "permissions".

---
+++ Conf/Defaults.cs
@@ -0,0 +1,13 @@
+﻿namespace EasyAuth.Conf
+{
+    /// <summary>
+    /// Provides default configuration values for the EasyAuth library.
+    /// </summary>
+    public static class Defaults
+    {
+        /// <summary>
+        /// The claim type used to store permissions.
+        /// </summary>
+        public const string PermissionClaimType = "permissions";
+    }
+}
