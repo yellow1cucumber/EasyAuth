90a47533e2b433b6e473bf2d5e9028b4096210b0
yellow1cucumber <sadovskiy1n@gmail.com>
2025-04-28 21:37:00 +0500

    feat: add user permissions service interface and implementation
    - Added `IUserPermissionsService` interface with methods
      for checking user permissions: `HasPermission` and
      `HasAnyPermission`.
    
    - Implemented `UserPermissionService` class that
      provides the functionality of the `IUserPermissionsService`
      interface, utilizing claims from a `ClaimsPrincipal`.
    
    - Fixed an issue in `HasAnyPermission` method where it
      incorrectly checked permissions against the input array
      instead of the user's permissions.

---
+++ Services/UserPermissions/UserPermissionService.cs
@@ -0,0 +1,49 @@
+﻿using System.Security.Claims;
+
+using EasyAuth.Conf;
+
+namespace EasyAuth.Services.UserPermissions
+{
+    /// <summary>
+    /// Implementation of <see cref="IUserPermissionsService"/> that checks user permissions based on claims.
+    /// </summary>
+    public sealed class UserPermissionService
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="UserPermissionService"/> class.
+        /// </summary>
+        /// <param name="claims">The claims principal representing the user.</param>
+        public UserPermissionService(ClaimsPrincipal claims)
+        {
+            this.userClaims = claims;
+
+            this.userPermissions = this.userClaims.Claims
+                                                  .Where(c => c.Type == Defaults.PermissionClaimType)
+                                                  .Select(c => c.Value)
+                                                  .ToHashSet();
+        }
+
+        private readonly ClaimsPrincipal userClaims;
+        private readonly HashSet<string> userPermissions;
+
+        /// <summary>
+        /// Checks if the user has a specific permission.
+        /// </summary>
+        /// <param name="permission">The permission to check.</param>
+        /// <returns>True if the user has the specified permission; otherwise, false.</returns>
+        public bool HasPermission(string permission)
+        {
+            return this.userPermissions.Contains(permission);
+        }
+
+        /// <summary>
+        /// Checks if the user has any of the specified permissions.
+        /// </summary>
+        /// <param name="permissions">The permissions to check.</param>
+        /// <returns>True if the user has at least one of the specified permissions; otherwise, false.</returns>
+        public bool HasAnyPermission(params string[] permissions)
+        {
+            return permissions.Any(p => permissions.Contains(p));
+        }
+    }
+}
